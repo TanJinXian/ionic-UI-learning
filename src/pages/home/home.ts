b29a51f721c4b0008ef53f06e001564ec5e0d50e
Jin Xian <jinxian@Jins-MacBook-Air.local>
2019-02-09 00:45:24 +0800

    until the basic interaction

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  username: string = "";
+  password: string = "";
+  userType: any;
+  gender: any;
+  notification: boolean;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  login() {
+    console.log("Hello");
+
+    console.log(this.username);
+    console.log(this.password);
+    console.log(this.userType);
+    console.log(this.gender);
+    console.log(this.notification);
+  }
+
+  cancel() {
+    this.username = "";
+    this.password = "";
   }
 
 }
